907e73cb2a90fda466bca3dd2e870ca5d5e29500
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-03-19 05:06:52 +0000

    include extra modules in kochanski too

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 KOCHANSKI_HOME=$(dirname $0)
 . ${KOCHANSKI_HOME}/etc/common
 
+for x in ${KOCHANSKI_EXTRAS}; do
+	${KOCHANSKI_HOME}/extras/${x}/build.sh $*
+	[ $? != 0 ] && echo "ERROR!!!" && exit 1
+done
+
 cd ${KOCHANSKI_HOME}
 
 TARGET="$1"
