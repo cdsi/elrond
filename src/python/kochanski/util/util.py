edde8a9dbc8e96443ac425769a39eedeaf19974e
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-03-19 01:18:27 +0000

    oops

--- src/python/kochanski/util/util.py
+++ src/python/kochanski/util/util.py
@@ -1,4 +1,3 @@
-import decorator
 import time
 
 def Property(f):
@@ -14,7 +13,6 @@ class Benchmark(Object):
     enabled = False
     categories = set([])
 
-    @decorator
     def __call__(self, __f):
         if not self.enabled or not self.__category & self.categories:
             return __f
@@ -26,7 +24,7 @@ class Benchmark(Object):
             print 'benchmark: call took %f s: %s' % (__t2 - __t1, __f.__name__)
             return __rc
 
-        # __benchmark.__doc__ = __f.__doc__
+        __benchmark.__doc__ = __f.__doc__
         return __benchmark
 
     def __init__(self, category=None):
