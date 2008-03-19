c43eff38d41e351f3364651a6d56d2add96f3e7b
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-03-19 01:47:40 +0000

    let install path be set by top-level module
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@5 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 KOCHANSKI_HOME=$(dirname $0)
 . ${KOCHANSKI_HOME}/etc/common
 
+cd ${KOCHANSKI_HOME}
+
 TARGET="$1"
 TARGET=${TARGET:="test"}
 
@@ -10,3 +12,4 @@ db-delete.sh
 db-create.sh
 
 ${PYTHON} ${PYTHONFLAGS} setup.py ${TARGET}
+[ $? != 0 ] && echo "ERROR!!!" && exit 1
