0fafe07fe0248321030dd2383dcbaec44dc8d521
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-04-03 07:06:27 +0000

    built-in python mode in emacs 22 has updated variable names

--- src/python/elrond/util.py
+++ src/python/elrond/util.py
@@ -121,8 +121,7 @@ class Prefs(Object):
 
 # Local Variables:
 # indent-tabs-mode: nil
-# py-indent-offset: 8
-# py-smart-indentation: nil
-# tab-width: 8
+# python-continuation-offset: 2
+# python-indent: 8
 # End:
 # vim: ai et si sw=8 ts=8
