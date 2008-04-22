e395633168fa75f2138ba6e06eaa6313691ef1dc
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-04-22 23:42:14 +0000

    oops

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ ELROND_HOME=$(dirname $0)
 
 cd ${ELROND_HOME}
 
+libtoolize --automake --copy --force
 aclocal -I ${ELROND_M4}
+autoheader
 automake --add-missing --copy --force-missing --foreign
 autoconf
