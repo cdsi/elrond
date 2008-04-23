e1d8be24265ab36cbfbea54b0b3b1fa4439ad58e
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-04-23 06:59:34 +0000

    this matches "log level" usage patterns elsewhere...

--- src/xul/xul.h
+++ src/xul/xul.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <glib.h>
 
-#define XUL_MAGIC 0xDEADBEEF
+#define XUL_MAGIC 0x1EADBEEF
 
 typedef enum {
         XUL_SUCCESS = 0,
@@ -12,8 +12,34 @@ typedef enum {
         XUL_EFOPEN = 2,
 } xul_rc_e;
 
+#define XUL_VERBOSE_DOMAIN "XUL"
+
+typedef enum {
+        XUL_VERBOSE_LEVEL_0 = 1 << (G_LOG_LEVEL_USER_SHIFT + 0),
+        XUL_VERBOSE_LEVEL_1 = 1 << (G_LOG_LEVEL_USER_SHIFT + 1),
+        XUL_VERBOSE_LEVEL_2 = 1 << (G_LOG_LEVEL_USER_SHIFT + 2),
+        XUL_VERBOSE_LEVEL_3 = 1 << (G_LOG_LEVEL_USER_SHIFT + 3),
+        XUL_VERBOSE_LEVEL_4 = 1 << (G_LOG_LEVEL_USER_SHIFT + 4),
+        XUL_VERBOSE_LEVEL_5 = 1 << (G_LOG_LEVEL_USER_SHIFT + 5),
+        XUL_VERBOSE_LEVEL_6 = 1 << (G_LOG_LEVEL_USER_SHIFT + 6),
+        XUL_VERBOSE_LEVEL_7 = 1 << (G_LOG_LEVEL_USER_SHIFT + 7),
+        XUL_VERBOSE_LEVEL_8 = 1 << (G_LOG_LEVEL_USER_SHIFT + 8),
+        XUL_VERBOSE_LEVEL_9 = 1 << (G_LOG_LEVEL_USER_SHIFT + 9),
+} xul_verbose_level_e;
+
+#define xul_verbose_log_0(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_0, __VA_ARGS__)
+#define xul_verbose_log_1(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_1, __VA_ARGS__)
+#define xul_verbose_log_2(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_2, __VA_ARGS__)
+#define xul_verbose_log_3(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_3, __VA_ARGS__)
+#define xul_verbose_log_4(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_4, __VA_ARGS__)
+#define xul_verbose_log_5(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_5, __VA_ARGS__)
+#define xul_verbose_log_6(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_6, __VA_ARGS__)
+#define xul_verbose_log_7(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_7, __VA_ARGS__)
+#define xul_verbose_log_8(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_8, __VA_ARGS__)
+#define xul_verbose_log_9(...) g_log(XUL_VERBOSE_DOMAIN, XUL_VERBOSE_LEVEL_9, __VA_ARGS__)
+
 typedef struct {
-        uint32_t level;
+        xul_verbose_level_e level;
         FILE *fp;
 } xul_verbose_t;
 
@@ -26,19 +52,6 @@ typedef struct {
 #define XUL_IS_VALID(x) ((x) && ((x)->magic == XUL_MAGIC))
 
 #define XUL_IS_SUCCESS(x) ((x) && ((x)->rc == XUL_SUCCESS))
-
-#define XUL_VERBOSE_LEVEL_ERROR G_LOG_LEVEL_ERROR
-#define XUL_VERBOSE_LEVEL_CRITICAL G_LOG_LEVEL_CRITICAL
-#define XUL_VERBOSE_LEVEL_WARNING G_LOG_LEVEL_WARNING
-#define XUL_VERBOSE_LEVEL_MESSAGE G_LOG_LEVEL_MESSAGE
-#define XUL_VERBOSE_LEVEL_INFO G_LOG_LEVEL_INFO
-#define XUL_VERBOSE_LEVEL_DEBUG G_LOG_LEVEL_DEBUG
-
-#define xul_verbose_message g_message
-#define xul_verbose_warning g_warning
-#define xul_verbose_critical g_critical
-#define xul_verbose_error g_error
-#define xul_verbose_debug g_debug
 
 #define XUL_APIEXPORT
 
