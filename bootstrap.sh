02a84468f36433f3edcbe6571a099075b440adfd
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-04-29 07:26:35 +0000

    extend the shared component module to also contain the external dependency list
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@84 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,6 @@ ELROND_HOME=$(dirname $0)
 cd ${ELROND_HOME}
 
 libtoolize --automake --copy --force
-ACLOCALFLAGS=
-XXX="/opt/libtool/share/aclocal /opt/pkg-config/share/aclocal /opt/glib/share/aclocal ${ELROND_M4}"
-for x in ${XXX}; do
-        if [ ! -d ${x} ]; then
-                continue
-        fi
-        ACLOCALFLAGS="-I ${x} ${ACLOCALFLAGS}"
-done
 aclocal ${ACLOCALFLAGS}
 autoheader
 automake --add-missing --copy --force-missing --foreign
