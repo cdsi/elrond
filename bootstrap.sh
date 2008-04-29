37b4f1e5e5ec2e791edbf568bfc2dd897d602fe4
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-04-29 00:04:01 +0000

    oops
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@82 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,15 @@ ELROND_HOME=$(dirname $0)
 cd ${ELROND_HOME}
 
 libtoolize --automake --copy --force
-aclocal -I /opt/libtool/share/aclocal -I /opt/pkg-config/share/aclocal -I /opt/glib/share/aclocal -I ${ELROND_M4}
+ACLOCALFLAGS=
+XXX="/opt/libtool/share/aclocal /opt/pkg-config/share/aclocal /opt/glib/share/aclocal ${ELROND_M4}"
+for x in ${XXX}; do
+        if [ ! -d ${x} ]; then
+                continue
+        fi
+        ACLOCALFLAGS="-I ${x} ${ACLOCALFLAGS}"
+done
+aclocal ${ACLOCALFLAGS}
 autoheader
 automake --add-missing --copy --force-missing --foreign
 autoconf
