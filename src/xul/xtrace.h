ef7c2bb40942261f03b352cb760c541ad41ba5bd
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-05-18 23:26:50 +0000

    added trace routine
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@116 f3b90fed-20f5-0310-8406-f4dde2dd57b2

---
+++ src/xul/xtrace.h
@@ -0,0 +1,16 @@
+#ifndef XTRACE_H
+#define XTRACE_H 1
+
+XUL_APIEXPORT void xul_xtrace(xul_t *, const char *, ...);
+
+#endif                          /* XTRACE_H */
+
+/*
+ * $Id:$
+ *
+ * Local Variables:
+ * c-file-style: "linux"
+ * indent-tabs-mode: nil
+ * End:
+ * vim: ai et si sw=8 ts=8
+ */
