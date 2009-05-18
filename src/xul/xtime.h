0523d7f14efcf160efa320d54f0f7c64b119e91b
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-05-18 23:27:46 +0000

    break out time prototypes

---
+++ src/xul/xtime.h
@@ -0,0 +1,17 @@
+#ifndef XTIME_H
+#define XTIME_H 1
+
+XUL_APIEXPORT gdouble xul_xtime(xul_t *, gdouble);
+XUL_APIEXPORT gdouble xul_xtime_overhead(xul_t *);
+
+#endif                          /* XTIME_H */
+
+/*
+ * $Id:$
+ *
+ * Local Variables:
+ * c-file-style: "linux"
+ * indent-tabs-mode: nil
+ * End:
+ * vim: ai et si sw=8 ts=8
+ */
