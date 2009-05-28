8333dbf2082b7572d84de3578fb500492de21788
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-05-28 03:22:24 +0000

    probably should be elsewhere

---
+++ src/python/elrond/macros.py
@@ -0,0 +1,16 @@
+def clamp(x, l=None, u=None):
+        """return value limited by lower and/or upper bounds"""
+
+        if u is not None:
+                x = min(x, u)
+        if l is not None:
+                x = max(x, l)
+
+        return x
+
+# Local Variables:
+# indent-tabs-mode: nil
+# python-continuation-offset: 2
+# python-indent: 8
+# End:
+# vim: ai et si sw=8 ts=8
