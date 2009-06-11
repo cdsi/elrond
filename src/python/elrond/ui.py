b9b8e6ac530addb8269c8b2652e9d23842ac03b1
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-06-11 18:47:33 +0000

    more resuable components

--- src/python/elrond/ui.py
+++ src/python/elrond/ui.py
@@ -3,6 +3,7 @@ from __future__ import with_statement
 
 import os
 import thread
+import threading
 import time
 
 from configobj import ConfigObj
@@ -11,6 +12,49 @@ import gobject
 import gtk
 
 from elrond.util import Object
+
+class Task(Object):
+        """http://unpythonic.blogspot.com/2007/08/using-threads-in-pygtk.html
+        """
+
+        def __loop(self, rc):
+                if rc is None:
+                        rc = ()
+                if not isinstance(rc, tuple):
+                        rc = (rc,)
+                self.callback(*rc)
+
+        def __start(self, *args, **kwargs):
+                for rc in self.generator(*args, **kwargs):
+                        if self.callback is not None:
+                                gobject.idle_add(self.__loop, rc)
+                if self.complete is not None:
+                        gobject.idle_add(self.complete)
+
+        def start(self, *args, **kwargs):
+                if self.__running:
+                        return
+
+                self.__running = True
+                thread = threading.Thread(target=self.__start, args=args, kwargs=kwargs)
+                thread.start()
+
+        def stop(self):
+                if not self.__running:
+                        return
+
+                self.__running = False
+
+        def kill(self):
+                self.stop()
+                thread.exit()
+
+        def __init__(self, generator, callback=None, complete=None):
+                self.generator = generator
+                self.callback = callback
+                self.complete = complete
+
+                self.__running = False
 
 class Chooser(gtk.FileChooserDialog):
 
