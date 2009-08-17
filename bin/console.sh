09ef23ab13c774030152af0478010e7722604b2a
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-08-17 21:40:20 +0000

    provide option to restore defaults
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@182 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- bin/console.sh
+++ bin/console.sh
@@ -5,4 +5,17 @@ export ELROND_HOME
 
 . "${ELROND_HOME}"/etc/common
 
+OPTION="$1"
+
+case "${OPTION}" in
+        *restore-defaults)
+                rm -f "${ELROND_ETC}"/console.ini
+                shift
+        ;;
+esac
+
+if [ ! -f "${FARAMIR_ETC}"/console.ini ]; then
+        cp "${FARAMIR_ETC}"/console.ini.in "${FARAMIR_ETC}"/console.ini
+fi
+
 exec python.sh "${ELROND_BIN}"/console.py "$@"
