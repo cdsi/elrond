63614986b525704a198c9b73b4c748a2cdf42da6
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-08-17 20:55:56 +0000

    so that other non-interactive tools may invoke a build

--- build-local.sh
+++ build-local.sh
@@ -3,13 +3,15 @@
 ELROND_HOME=$(dirname $0)
 . ${ELROND_HOME}/etc/common
 
-echo -n "Delete the database? [y/N] "
-read answer
-case "${answer}" in
-	[yY]*)
-                elrond-db-delete.sh
-	;;
-esac
+if [ ! "${SKIP_QUESTIONS}" = "" ]; then
+        echo -n "Delete the database? [y/N] "
+        read answer
+        case "${answer}" in
+                [yY]*)
+                        elrond-db-delete.sh
+                ;;
+        esac
+fi
 
 elrond-db-load.sh
 # TODO: [ $? != 0 ] && exit 1
