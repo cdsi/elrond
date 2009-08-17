0cec0153f77a307a4e2d27fb020c81db4435e829
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-08-17 21:40:20 +0000

    provide option to restore defaults

--- bin/dialog.sh
+++ bin/dialog.sh
@@ -5,4 +5,17 @@ export ELROND_HOME
 
 . "${ELROND_HOME}"/etc/common
 
+OPTION="$1"
+
+case "${OPTION}" in
+        *restore-defaults)
+                rm -f "${ELROND_ETC}"/dialog.ini
+                shift
+        ;;
+esac
+
+if [ ! -f "${FARAMIR_ETC}"/dialog.ini ]; then
+        cp "${FARAMIR_ETC}"/dialog.ini.in "${FARAMIR_ETC}"/dialog.ini
+fi
+
 exec python.sh "${ELROND_BIN}"/dialog.py "$@"
