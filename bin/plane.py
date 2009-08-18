a6efa4d1d4a81f331f9ec19196e82cadd80f0442
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-08-18 03:07:04 +0000

    add re-usable cartesian plane widget
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@184 f3b90fed-20f5-0310-8406-f4dde2dd57b2

---
+++ bin/plane.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from elrond.ui import Plane
+
+if __name__ == '__main__':
+
+        plane = Plane()
+
+        plane.show()
+        plane.run()
+
+# $Id:$
+#
+# Local Variables:
+# indent-tabs-mode: nil
+# python-continuation-offset: 2
+# python-indent: 8
+# End:
+# vim: ai et si sw=8 ts=8
