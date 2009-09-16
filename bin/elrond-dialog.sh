b3fc4c1cca965a79723d6ab80552040209a3dc7e
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-09-16 21:45:39 +0000

    namespace protect

---
+++ bin/elrond-dialog.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ELROND_HOME="$(dirname "$0")"/..
+export ELROND_HOME
+
+. "${ELROND_HOME}"/etc/common
+
+OPTION="$1"
+
+case "${OPTION}" in
+        *restore-defaults)
+                rm -f "${ELROND_ETC}"/elrond-dialog.ini
+                shift
+        ;;
+esac
+
+if [ ! -f "${ELROND_ETC}"/elrond-dialog.ini ]; then
+        cp "${ELROND_ETC}"/elrond-dialog.ini.in "${ELROND_ETC}"/elrond-dialog.ini
+fi
+
+exec python.sh "${ELROND_BIN}"/elrond-dialog.py "$@"
