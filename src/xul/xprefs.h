da04f4ef424fb277f65f2fc4ecb47737e4bbc4a6
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-09-30 17:07:47 +0000

    oops
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@202 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- src/xul/xprefs.h
+++ src/xul/xprefs.h
@@ -25,8 +25,8 @@ XUL_APIEXPORT gdouble xul_prefs_gdouble_get(xul_t *, const gchar *, const gchar 
 XUL_APIEXPORT void xul_prefs_gdouble_set(xul_t *, const gchar *, const gchar *, gdouble);
 XUL_APIEXPORT gboolean xul_prefs_gboolean_get(xul_t *, const gchar *, const gchar *);
 XUL_APIEXPORT void xul_prefs_gboolean_set(xul_t *, const gchar *, const gchar *, gboolean);
-XUL_APIEXPORT const gchar *xul_prefs_gchar_get(xul_t *, const gchar *, const gchar *, gchar *, gsize);
-XUL_APIEXPORT void xul_prefs_gchar_set(xul_t *, const gchar *, const gchar *, const gchar *, gsize);
+XUL_APIEXPORT const gchar *xul_prefs_gstring_get(xul_t *, const gchar *, const gchar *, gchar *, gsize);
+XUL_APIEXPORT void xul_prefs_gstring_set(xul_t *, const gchar *, const gchar *, const gchar *, gsize);
 XUL_APIEXPORT void xul_prefs_close(xul_t *);
 XUL_APIEXPORT xul_rc_e xul_prefs_open(xul_t *, const gchar *);
 
