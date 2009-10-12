1be8185600140fc1f95e2b620b30619f503a006b
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-10-12 19:38:57 +0000

    standalone save as... dialog

---
+++ bin/elrond-saveas.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from elrond.ui import SaveAs
+
+if __name__ == '__main__':
+
+        saveas = SaveAs()
+
+        saveas.show()
+        saveas.run()
+
+# $Id:$
+#
+# Local Variables:
+# indent-tabs-mode: nil
+# python-continuation-offset: 2
+# python-indent: 8
+# End:
+# vim: ai et si sw=8 ts=8
