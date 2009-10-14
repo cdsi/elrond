8072d2bbafabfd28eec1d0d1e5b007ea03573c65
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-10-14 23:35:45 +0000

    use a callback to act upon user selection

--- bin/elrond-saveas.py
+++ bin/elrond-saveas.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
+import os
+
 from elrond.ui import SaveAs
+
+def selection_cb(selection):
+        print 'selection =', selection
 
 if __name__ == '__main__':
 
-        saveas = SaveAs()
+        chooser = SaveAs()
 
-        saveas.show()
-        saveas.run()
+        chooser.callback = selection_cb
+        chooser.get_selection(path=os.environ['ELROND_HOME'], filename='build.sh')
+
+        chooser.show()
+        chooser.run()
 
 # $Id:$
 #
