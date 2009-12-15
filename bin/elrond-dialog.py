ca8339844f6657d648578f34d1ad8934bc7aadf7
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-12-15 18:49:53 +0000

    more pythonic

--- bin/elrond-dialog.py
+++ bin/elrond-dialog.py
@@ -4,36 +4,35 @@ from optparse import OptionParser
 
 from elrond.ui import Dialog
 
-if __name__ == '__main__':
-        op = OptionParser('%prog [options]')
+op = OptionParser('%prog [options]')
 
-        op.add_option('--socket', action='store', dest='socket',
-                      help='The named pipe created by mkfifo')
-        op.add_option('--labels', action='store', dest='labels',
-                      help='label1:label2:label3 etc...')
+op.add_option('--socket', action='store', dest='socket',
+              help='The named pipe created by mkfifo')
+op.add_option('--labels', action='store', dest='labels',
+              help='label1:label2:label3 etc...')
 
-        op.add_option('--title', action='store', dest='title', default=None,
-                      help='The dialog window title.')
-        op.add_option('--deletable', action='store', dest='deletable', default=True,
-                      help='When disabled the dialog window is not closable.')
+op.add_option('--title', action='store', dest='title', default=None,
+              help='The dialog window title.')
+op.add_option('--deletable', action='store', dest='deletable', default=True,
+              help='When disabled the dialog window is not closable.')
 
-        (options, args) = op.parse_args()
+(options, args) = op.parse_args()
 
-        if options.socket == None:
-                op.error('--socket=... is required')
-        if options.labels == None:
-                op.error('--labels=... is required')
+if options.socket == None:
+        op.error('--socket=... is required')
+if options.labels == None:
+        op.error('--labels=... is required')
 
-        dialog = Dialog()
+dialog = Dialog()
 
-        dialog.socket = options.socket
-        dialog.labels = options.labels
+dialog.socket = options.socket
+dialog.labels = options.labels
 
-        dialog.title = options.title
-        dialog.deletable = options.deletable
+dialog.title = options.title
+dialog.deletable = options.deletable
 
-        dialog.show()
-        dialog.run()
+dialog.show()
+dialog.run()
 
 # $Id:$
 #
