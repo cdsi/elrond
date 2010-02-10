0598f13f24985d153f65e930c003ceb4b2aa765e
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2010-02-10 21:59:05 +0000

    top-level window widget to hold all other widgets
    git-svn-id: file:///home/tvaughan/Projects/elrond/svn/trunk@262 f3b90fed-20f5-0310-8406-f4dde2dd57b2

---
+++ bin/elrond-window.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+
+from elrond.ui import Window
+
+op = OptionParser('%prog [options]')
+
+op.add_option('--title', action='store', dest='title', default=None,
+              help='The window title.')
+op.add_option('--deletable', action='store', dest='deletable', default=True,
+              help='When disabled the window is not closable.')
+
+(options, args) = op.parse_args()
+
+window = Window()
+
+window.title = options.title
+window.deletable = options.deletable
+
+window.show()
+window.run()
+
+# $Id:$
+#
+# Local Variables:
+# indent-tabs-mode: nil
+# python-continuation-offset: 2
+# python-indent: 8
+# End:
+# vim: ai et si sw=8 ts=8
