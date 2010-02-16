2924fc73d2291da66beacc32f7ab3d847aa66e8f
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2010-02-16 00:00:27 +0000

    cosmetic

--- src/python/elrond/tasks.py
+++ src/python/elrond/tasks.py
@@ -27,18 +27,18 @@ class Task(Object):
                         gobject.idle_add(self.complete)
 
         def start(self, *args, **kwargs):
-                if self.__running:
+                if self.__is_running:
                         return
 
-                self.__running = True
+                self.__is_running = True
                 thread = threading.Thread(target=self.__start, args=args, kwargs=kwargs)
                 thread.start()
 
         def stop(self):
-                if not self.__running:
+                if not self.__is_running:
                         return
 
-                self.__running = False
+                self.__is_running = False
 
         def kill(self):
                 self.stop()
@@ -49,7 +49,7 @@ class Task(Object):
                 self.callback = callback
                 self.complete = complete
 
-                self.__running = False
+                self.__is_running = False
 
 # $Id:$
 #
