9a6eb7736cacab8fa4c137c35cb012464e312cf4
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2010-02-23 04:14:07 +0000

    theoden updates

---
+++ bin/lighttpd.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+ELROND_HOME=$(dirname $0)/..
+export ELROND_HOME
+
+. ${ELROND_HOME}/etc/common
+
+print_usage_and_die()
+{
+    echo "Usage: $(basename $0) [--start | --stop | --make-cert]"
+    exit 1
+}
+
+OPTION="$1"
+
+case "${OPTION}" in
+    *start)
+	lighttpd -f ${ELROND_ETC}/lighttpd.conf
+	;;
+    *stop)
+	[ -f ${ELROND_RUN}/lighttpd.pid ] && \
+	    kill $(cat ${ELROND_RUN}/lighttpd.pid)
+	;;
+                
+    *make-cert)
+	/usr/sbin/make-ssl-cert /usr/share/ssl-cert/ssleay.cnf \
+	    ${ELROND_ETC}/lighttpd.pem --force-overwrite
+	;;
+    *)
+	print_usage_and_die
+	;;
+esac
