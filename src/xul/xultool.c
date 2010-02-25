3ff9762d5c22a613031e3fb52fa229770621795b
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2010-02-25 23:58:23 +0000

    move prefenece tests into unit test harness

--- src/xul/xultool.c
+++ src/xul/xultool.c
@@ -16,8 +16,6 @@
 
 #define XULTOOL_ERROR xul_error_domain_create("xultool")
 
-#define xul_verbose_log_prefs xul_verbose_log_0x0001
-
 typedef enum {
         XULTOOL_ERROR_FOO,
         XULTOOL_ERROR_BAR,
@@ -142,49 +140,6 @@ main(int argc, char **argv)
         }
 
         xul_verbose_log("verbose mask = 0x%0" G_GINT32_MODIFIER "X", verbose);
-
-        const gchar *prefs = g_strconcat(getenv("ELROND_ETC"), G_DIR_SEPARATOR_S, "xultool.ini", NULL);
-
-        rc = xul_prefs_open(xul, prefs);
-        if (rc) {
-                if (XUL_IS_ERROR(xul)) {
-                        g_fprintf(stderr, "ERROR: %s\n", xul_error_message_get(xul));
-                }
-                return 1;
-        }
-
-        gint64 int64 = xul_prefs_gint64_get(xul, "data", "int64");
-        xul_verbose_log_prefs("int64 = %" G_GINT64_FORMAT, int64);
-
-        guint64 uint64 = xul_prefs_guint64_get(xul, "data", "uint64");
-        xul_verbose_log_prefs("uint64 = %" G_GUINT64_FORMAT, uint64);
-
-        gint32 int32 = xul_prefs_gint32_get(xul, "data", "int32");
-        xul_verbose_log_prefs("int32 = %" G_GINT32_FORMAT, int32);
-
-        guint32 uint32 = xul_prefs_guint32_get(xul, "data", "uint32");
-        xul_verbose_log_prefs("uint32 = %" G_GUINT32_FORMAT, uint32);
-
-        gint64 hex64 = xul_prefs_ghex64_get(xul, "data", "hex64");
-        xul_verbose_log_prefs("hex64 = 0x%0" G_GINT64_MODIFIER "X", hex64);
-
-        guint64 uhex64 = xul_prefs_guhex64_get(xul, "data", "uhex64");
-        xul_verbose_log_prefs("uhex64 = 0x%0" G_GINT64_MODIFIER "X", uhex64);
-
-        gint32 hex32 = xul_prefs_ghex32_get(xul, "data", "hex32");
-        xul_verbose_log_prefs("hex32 = 0x%0" G_GINT32_MODIFIER "X", hex32);
-
-        guint32 uhex32 = xul_prefs_guhex32_get(xul, "data", "uhex32");
-        xul_verbose_log_prefs("uhex32 = 0x%0" G_GINT32_MODIFIER "X", uhex32);
-
-        gdouble dubell = xul_prefs_gdouble_get(xul, "data", "double");
-        xul_verbose_log_prefs("double = %lf", dubell);
-
-        gboolean bool = xul_prefs_gboolean_get(xul, "data", "boolean");
-        xul_verbose_log_prefs("boolean = %s", bool ? "TRUE" : "FALSE");
-
-        const gchar *string = xul_prefs_gstring_get(xul, "data", "string", &g_buffer[0], sizeof(g_buffer));
-        xul_verbose_log_prefs("string = %s", string);
 
         rc = test_foo(xul);
         if (rc) {
