3874dc606a65f4b67c916be915fd2cfb78cb1311
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2010-05-18 23:31:39 +0000

    per pylint...

--- src/python/elrond/static.py
+++ src/python/elrond/static.py
@@ -1,6 +1,21 @@
 from __future__ import division
 from __future__ import with_statement
 
+HEXFILT = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
+
+def hexdump(src, size, length=8):
+        """ http://code.activestate.com/recipes/142812/ """
+
+        result = []
+
+        for i in xrange(0, size, length):
+                sub1 = src[i:i + length]
+                hexa = ' '.join(['%02X' % ord(x) for x in sub1])
+                sub2 = sub1.translate(HEXFILT)
+                result.append('%04X   %-*s   %s\n' % (i, length * 3, hexa, sub2))
+
+        return ''.join(result)
+
 def reverse_bits(value, nbits=32):
         """ Reverse the bits in an int. Assume 32-bits by default. """
 
