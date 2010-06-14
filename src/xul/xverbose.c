435fadad0cea6a8f782f752b092c6c001cae63ae
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2010-06-14 22:22:52 +0000

    quick and dirty hack per the tower track test at ft. sill

--- src/xul/xverbose.c
+++ src/xul/xverbose.c
@@ -215,7 +215,7 @@ xul_verbose_init(xul_t * xul)
         g_assert(XUL_VERBOSE_IS_VALID(verbose));
 
         xul_verbose_mask_set(xul, XUL_VERBOSE_MASK_0x0000);
-        xul_verbose_filter_set(xul, xul_verbose_filter_default);
+        xul_verbose_filter_set(xul, xul_verbose_filter_time0);
         xul_verbose_log_open(xul, "/dev/stdout");
 }
 
