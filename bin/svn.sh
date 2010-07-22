e44fd0ba99ecfd32a0457f8928de7123684fe54c
Tom Vaughan <tom@software6.net>
2010-07-22 08:10:23 -0700

    obsolete

--- bin/svn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ELROND_HOME=$(dirname $0)/..
-export ELROND_HOME
-
-. ${ELROND_HOME}/etc/common
-
-elrond_record ${SVN} ${SVNFLAGS} ${ELROND_SVNFLAGS} "$@"
-exec ${SVN} ${SVNFLAGS} ${ELROND_SVNFLAGS} "$@"
