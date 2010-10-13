bc3ad4518222e09b33e666245a9ec799210e7dac
Tom Vaughan <tom@software6.net>
2010-10-13 11:21:44 -0700

    java is obsolete...

--- bin/java.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ELROND_HOME=$(dirname $0)/..
-export ELROND_HOME
-
-. ${ELROND_HOME}/etc/common
-
-elrond_record ${JAVA} ${JAVAFLAGS} ${ELROND_JAVAFLAGS} "$@"
-exec ${JAVA} ${JAVAFLAGS} ${ELROND_JAVAFLAGS} "$@"
--- bin/netbeans.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ELROND_HOME=$(dirname $0)/..
-export ELROND_HOME
-
-. ${ELROND_HOME}/etc/common
-
-elrond_record ${NETBEANS} ${NETBEANSFLAGS} "$@"
-exec ${NETBEANS} ${NETBEANSFLAGS} "$@"
