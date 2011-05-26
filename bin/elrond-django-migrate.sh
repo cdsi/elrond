2b786217c28a7dfeb0aea8bf99ebcbc9cb6dbb12
Tom Vaughan <tom@software6.net>
2011-05-26 11:40:41 -0700

    import latest theoden updates

--- bin/elrond-django-migrate.sh
+++ bin/elrond-django-migrate.sh
@@ -6,4 +6,4 @@ export ELROND_HOME
 . ${ELROND_HOME}/etc/common
 
 elrond-django-manage.sh syncdb --noinput
-elrond-django-manage.sh migrate elrond.www.impex
+# TODO: elrond-django-manage.sh migrate elrond.www.appname
