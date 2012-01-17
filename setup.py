19a891c868575f1504b8da4f5e12b8d6e7ba854a
Bassem Ali <bassem.ali@gmail.com>
2012-01-17 15:46:49 -0800

    Set up for use with distutils

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='elrond',
+    version='1.4.1',
+    description='Easy PyGTK+ widget creation',
+    author='CDSI',
+    author_email='elrond@creativedigitalsys.com',
+    url='http://www.creativedigitalsys.com',
+    package_dir={'': 'src/python'},
+    packages=['elrond'],
+)
